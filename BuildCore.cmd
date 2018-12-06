f2a504de90db5bc614cf75c3418f51d933b1c9df
Ryan Newton <rrnewton@gmail.com>
2018-12-06 10:55:19 -0800

    Restore BuildCore.cmd to its state that uses an absolute directory, e.g. C:\ambrosia

--- BuildCore.cmd
+++ BuildCore.cmd
@@ -1,4 +1,5 @@
-dotnet publish -o ./bin -c Release -f netcoreapp2.0 -r win10-x64 Ambrosia/Ambrosia/Ambrosia.csproj
-dotnet publish -o ./bin -c Release -f netcoreapp2.0 -r win10-x64 ImmortalCoordinator/ImmortalCoordinator.csproj
-dotnet publish -o ./bin -c Release -f netcoreapp2.0 -r win10-x64 Clients/CSharp/AmbrosiaCS/AmbrosiaCS.csproj
+dotnet publish -o /ambrosia/ambrosia/bin/x64/Release/netcoreapp2.0 -c Release -f netcoreapp2.0 -r win10-x64 Ambrosia/Ambrosia/Ambrosia.csproj
+dotnet publish -o /ambrosia/ambrosia/bin/x64/Release/netcoreapp2.0 -c Release -f netcoreapp2.0 -r win10-x64 ImmortalCoordinator/ImmortalCoordinator.csproj
+
+dotnet publish -o /ambrosia/ambrosia/bin/x64/Release/netcoreapp2.0 -c Release -f netcoreapp2.0 -r win10-x64 Clients/CSharp/AmbrosiaCS/AmbrosiaCS.csproj
 dotnet publish -o ./bin -c Release -f netcoreapp2.0 -r win10-x64 DevTools/UnsafeDeregisterInstance/UnsafeDeregisterInstance.csproj
