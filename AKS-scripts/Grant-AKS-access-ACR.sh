492288e64fa876c902e8b030ce411bed16132df9
Ryan Newton <rrnewton@gmail.com>
2018-12-06 13:36:17 -0800

    AKS-scripts: fix the Grant-AKS-access with a workaround for aggressive MSYS path conversion on Windows.

--- AKS-scripts/Grant-AKS-access-ACR.sh
+++ AKS-scripts/Grant-AKS-access-ACR.sh
@@ -28,9 +28,16 @@ CLIENT_ID=$($AZ aks show --resource-group $AZURE_RESOURCE_GROUP --name $AZURE_KU
 # Get the ACR registry resource id
 ACR_ID=$($AZ acr show --name $ACR_NAME --resource-group $AZURE_RESOURCE_GROUP --query "id" --output tsv)
 
+# On Bash on windows (as bundled with Git for windows) we have endless
+# headaches because of attempts to convert anything that starts with a
+# forward slash to a very long windows path.  This disables such
+# translation so that the "--scope=$ACR_ID" below is not mangled on Windows.
+# It should have no affect on Linux/MacOS.
+export MSYS_NO_PATHCONV=1
+
 # Set it to empty string if there's an error:
 EXISTING_ROLES=""
-EXISTING_ROLES=$($AZ role assignment list --role Reader --scope $ACR_ID --query '[0].id')
+EXISTING_ROLES=$($AZ role assignment list --role Reader "--scope=$ACR_ID" --query '[0].id')
 set +x
 echo
 
@@ -38,7 +45,7 @@ echo
 if [ "$EXISTING_ROLES" == "" ]; then
     echo "Creating new role assignment."
     set -x
-    $AZ role assignment create --assignee $CLIENT_ID --role Reader --scope $ACR_ID 
+    $AZ role assignment create --assignee $CLIENT_ID --role Reader "--scope=$ACR_ID"
     set +x
 else echo "Role assignments exists, ASSUMING it is correct."
 fi
