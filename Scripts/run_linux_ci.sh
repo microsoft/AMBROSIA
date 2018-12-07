a4094d2c71f4bc78e0f2457c4f442f1c08bb713d
Ryan Newton <rrnewton@gmail.com>
2018-12-07 09:54:44 -0800

    Hack CI script to deal with odd way Azure Devops runs the CI script (copying it out of place)

--- Scripts/run_linux_ci.sh
+++ Scripts/run_linux_ci.sh
@@ -3,9 +3,13 @@ set -xeu
 
 # A simple script to build and test under Linux CI.
 
-# Switch to the top of the working copy:
-cd `dirname $0`/../
-
+# Hack to deal with Azure Devops Pipelines:
+if ! [[ -e ./build_docker_images.sh ]]; then
+    # For MOST CI environments, running this script in-place, this
+    # will get us to the top of the repo:
+    cd `dirname $0`/../
+fi
+    
 # Gather a bit of info about where we are:
 uname -a
 pwd -P
