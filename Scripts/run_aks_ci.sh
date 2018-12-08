7c63838475ce1f659cfe913d9866edbd75afee65
Ryan Newton <ryan.newton@alum.mit.edu>
2018-12-08 00:21:41 -0800

    run_aks_ci: minor script tweak

--- Scripts/run_aks_ci.sh
+++ Scripts/run_aks_ci.sh
@@ -4,8 +4,8 @@ set -euo pipefail
 # This script runs an end-to-end Azure/Kubernetes test.
 
 echo "Running in directory: "`pwd`
+cd ../AKS-scripts
 echo "Filling settings into template (Defs/AmbrosiaAKSConf.sh)..."
-cd ./AKS-scripts
 cp Defs/AmbrosiaAKSConf.sh.template Defs/AmbrosiaAKSConf.sh
 sed -i 's|xyz|aksambrosiaci|'   Defs/AmbrosiaAKSConf.sh
 sed -i "s|PASTE_SUBSCRIPTION_ID_HERE|$AZURE_SUBSCRIPTION|" Defs/AmbrosiaAKSConf.sh
