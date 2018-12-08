eaaa02b8923a42b8cb8e0e9a6804fee60f09b44f
Ryan Newton <ryan.newton@alum.mit.edu>
2018-12-08 00:40:42 -0800

    Fix a regression in Create-AKS-SMBFileShare-Secret.  Update run_aks_ci

--- Scripts/run_aks_ci.sh
+++ Scripts/run_aks_ci.sh
@@ -3,8 +3,15 @@ set -euo pipefail
 
 # This script runs an end-to-end Azure/Kubernetes test.
 
+echo "Running in directory: "`pwd`
+if [[ -d `dirname $0`/AKS-scripts ]]; then
+    cd `dirname $0`/AKS-scripts
+elif [[ -d ../AKS-scripts ]]; then
+    cd ../AKS-scripts
+else 
+    cd ./AKS-scripts
+fi
 echo "Filling settings into template (Defs/AmbrosiaAKSConf.sh)..."
-pushd AKS-scripts
 cp Defs/AmbrosiaAKSConf.sh.template Defs/AmbrosiaAKSConf.sh
 sed -i 's|xyz|aksambrosiaci|'   Defs/AmbrosiaAKSConf.sh
 sed -i "s|PASTE_SUBSCRIPTION_ID_HERE|$AZURE_SUBSCRIPTION|" Defs/AmbrosiaAKSConf.sh
@@ -13,4 +20,7 @@ echo "Launching end-to-end test..."
 ./run-end-to-end-test.sh
 
 echo "Cleaning up..."
-./Clean-AKS.sh all
+# ./Clean-AKS.sh all
+
+# Leave the passive resources, but delete the active pods:
+kubectl delete pods,deployments --all
