1bf87361ac702de786fbe7b647e8e53f5ef5b4a3
Ryan Newton <ryan.newton@alum.mit.edu>
2018-12-08 10:36:02 -0800

    Major improvements in runAmbrosiaService.sh script.  Add health monitoring and fix logging redirection bugs.

--- Scripts/runAmbrosiaService.sh
+++ Scripts/runAmbrosiaService.sh
@@ -21,6 +21,8 @@ set -euo pipefail
 #
 # ----------------------------------------------------------
 
+TAG="[runAmbrosiaService.sh]"
+
 function print_usage() {
     echo "USAGE: $0 <service-binary> <service-args>*"
     echo    
@@ -45,7 +47,7 @@ function print_usage() {
 if [ $# -eq 0 ]; then print_usage; fi
 
 if [[ ! -v AMBROSIA_INSTANCE_NAME ]]; then
-    echo "ERROR: unbound environment variable: AMBROSIA_INSTANCE_NAME"
+    echo "ERROR $TAG: unbound environment variable: AMBROSIA_INSTANCE_NAME"
     echo 
     print_usage
     exit 1
@@ -53,13 +55,19 @@ fi
 
 if [[ -v AMBROSIA_IMMORTALCOORDINATOR_PORT ]];
 then
-    echo "Using environment var AMBROSIA_IMMORTALCOORDINATOR_PORT=$AMBROSIA_IMMORTALCOORDINATOR_PORT"
+    echo " $TAG Using environment var AMBROSIA_IMMORTALCOORDINATOR_PORT=$AMBROSIA_IMMORTALCOORDINATOR_PORT"
 else
     AMBROSIA_IMMORTALCOORDINATOR_PORT=1500
-    echo "Using default AMBROSIA_IMMORTALCOORDINATOR_PORT of $AMBROSIA_IMMORTALCOORDINATOR_PORT"
+    echo " $TAG Using default AMBROSIA_IMMORTALCOORDINATOR_PORT of $AMBROSIA_IMMORTALCOORDINATOR_PORT"
 fi
 
-COORDLOG=/var/log/ImmortalCoordinator.log
+if [[ "${AMBROSIA_IMMORTALCOORDINATOR_LOG:+defined}" ]]; then
+    COORDLOG="${AMBROSIA_IMMORTALCOORDINATOR_LOG}"
+    echo " $TAG Responding to env var AMBROSIA_IMMORTALCOORDINATOR_LOG=$COORDLOG"
+else
+    # The canonical location, especially when running in a Docker container:
+    COORDLOG=/var/log/ImmortalCoordinator.log
+fi
 
 # Arguments: all passed through to the coordinator.
 # Returns: when the Coordinator is READY (in the background).
@@ -70,58 +78,99 @@ COORDLOG=/var/log/ImmortalCoordinator.log
 # Side effect: uses a log file on disk in the same directory as this script.
 # Side effect: runs a tail proycess in the background
 function start_immortal_coordinator() {
-    echo "Launching coordingator with: ImmortalCoordinator" $*
-    echo "  Redirecting output to: $COORDLOG"
-
+    echo " $TAG Launching coordingator with: ImmortalCoordinator" $*
+    if ! which ImmortalCoordinator; then
+	echo "  ERROR $TAG - ImmortalCoordinator not found on path!"
+	exit 1
+    fi    
+    echo " $TAG   Creating zero length log file: $COORDLOG"
     if ! truncate -s 0 "$COORDLOG";
-    then COORDLOG="./ImmortalCoordinator.log"
-	 echo " ! WARNING [runAmbrosiaService.sh]: could not write coordinator log, using $COORDLOG instead."
-	 truncate -s 0 "$COORDLOG"
+    then
+	COORDLOG=`mktemp ImmortalCoordinator.XXXXXX.log`
+	echo " ! WARNING $TAG: could not write coordinator log, using $COORDLOG instead."
+	truncate -s 0 "$COORDLOG"
     fi
+    echo " $TAG   Redirecting output to: $COORDLOG"
     
     if which rotatelogs; then 
 	# Bound the total amount of output used by the ImmortalCoordinator log:
-	ImmortalCoordinator $* 2>1 | rotatelogs -f -t "$COORDLOG" 10M &
+	set -x 
+	ImmortalCoordinator $* 2>&1 | rotatelogs -f -t "$COORDLOG" 10M &
+	set +x
 	coord_pid=$!
     else
-	echo " ! WARNING [runAmbrosiaService.sh]: rotatelogs not available, NOT bounding $COORDLOG size."
-	ImmortalCoordinator $* 2>1 > "$COORDLOG" &
+	echo " ! WARNING $TAG: rotatelogs not available, NOT bounding size of $COORDLOG"
+	set -x
+	ImmortalCoordinator $* >>"$COORDLOG" 2>&1 &
+	set +x
 	coord_pid=$!
     fi
 
     while [ ! -e "$COORDLOG" ]; do
-	echo " -> Waiting for $COORDLOG to appear"
+	echo " $TAG -> Waiting for $COORDLOG to appear"
 	sleep 1
     done
     if [[ ! -v AMBROSIA_SILENT_COORDINATOR ]]; then
-	tail -F $COORDLOG | while read l; do echo " [ImmortalCoord] $l"; done &
+	# It seems like tail -F is spitting the same output multiple times on 
+	# A complicated but full-proof bash method of line-by-line reading:
+	tail -F "$COORDLOG" | \
+	  while IFS='' read -r line || [[ -n "$line" ]]; do
+	    echo " [ImmortalCoord] $line";
+	  done &
     fi
     while ! grep -q "Ready" "$COORDLOG" && kill -0 $coord_pid 2>/dev/null ;
     do sleep 2; done
 
     if ! kill -0 $coord_pid 2>/dev/null ;
     then echo
-	 echo "ERROR [runAmbrosiaService.sh]"
+	 echo "--------------- ERROR $TAG ----------------"
 	 echo "Coordinator died while we were waiting.  Final log ended with:"
 	 echo "--------------------------------------------------------------"
-	 tail $COORDLOG
+	 tail -n20 $COORDLOG
+	 echo "--------------------------------------------------------------"	 
 	 exit 1;
     fi
-    echo "Coordinator ready."
+    echo " $TAG Coordinator looks ready."
 }
 
-# TODO: health monitoring.  Show an example of AMBROSIA best practice here.
+# Health monitoring. This is an example of AMBROSIA required practice:
+# the coordinator and application are bound together, if one dies the
+# other must (as though they were one process.)
+# 
+keep_monitoring=`mktemp healthMonitorContinue.XXXXXX`
+touch $keep_monitoring
 
+function monitor_health() {
+    echo " $TAG Health monitor starting coord_pid=$coord_pid, app_pid=$app_pid"
+    while [ -f $keep_monitoring ]; do
+	sleep 2
+	if ! kill -0 $coord_pid 2>- ; then
+	    echo "-------------------------------------------------------"
+	    echo "ERROR $TAG ImmortalCoordinator died!"
+	    echo "Killing application process.  Must initiate recovery."
+	    echo "-------------------------------------------------------"
+	    set -x 
+	    kill -9 $app_pid
+	    exit 1
+	fi
+    done
+    echo " $TAG Cleanly exiting health monitor background function..."
+}
 
 # Step 1: 
 start_immortal_coordinator -i $AMBROSIA_INSTANCE_NAME -p $AMBROSIA_IMMORTALCOORDINATOR_PORT
 
 # Step 2:
-echo "Launching app client process:"
+echo " $TAG Launching app client process:"
 set -x
-$*
+$* &
 set +x
+app_pid=$!
 
-echo "Ambrosia: client exited, killing coordinator..."
-kill $coord_pid || echo ok
+monitor_health &
 
+wait $app_pid
+echo " $TAG Ambrosia: client exited, killing coordinator and health monitor function..."
+kill -9 $coord_pid || echo ok
+rm -f $keep_monitoring
+wait
