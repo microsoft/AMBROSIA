eab75e80a3005d244e822aa1fc5adceb0344523d
Ryan Newton <ryan.newton@alum.mit.edu>
2018-12-07 23:53:58 -0800

    Add a script for running full AKS test on ADO

---
+++ Scripts/run_aks_ci.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -euo pipefail
+
+# This script runs an end-to-end Azure/Kubernetes test.
+
+echo "Filling settings into template (Defs/AmbrosiaAKSConf.sh)..."
+pushd AKS-scripts
+cp Defs/AmbrosiaAKSConf.sh.template Defs/AmbrosiaAKSConf.sh
+sed -i 's|xyz|aksambrosiaci|'   Defs/AmbrosiaAKSConf.sh
+sed -i "s|PASTE_SUBSCRIPTION_ID_HERE|$AZURE_SUBSCRIPTION|" Defs/AmbrosiaAKSConf.sh
+
+echo "Launching end-to-end test..."
+./run-end-to-end-test.sh
+
+echo "Cleaning up..."
+./Clean-AKS.sh all
