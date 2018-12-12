f0e130e1d6600bb2fe14b414972839e664c90a6d
Ryan Newton <rrnewton@gmail.com>
2018-12-12 15:58:08 -0800

    HelloWorld: Finish up BASH how to guide

--- Samples/HelloWorld/run_helloworld_both.sh
+++ Samples/HelloWorld/run_helloworld_both.sh
@@ -15,18 +15,6 @@ set -euo pipefail
 # ------------------------------------------------------------------------------
 cd `dirname $0`
 
-# PORTOFFSET: A number to add to all ports to avoid colliding or
-# reusing recently used ports.
-# if ! [ ${OFFSET:+defined} ]; then
-#     OFFSET=0
-# fi
-# PORT1=$((1000 + OFFSET))
-# PORT2=$((1001 + OFFSET))
-# PORT3=$((1002 + OFFSET))
-# PORT4=$((1003 + OFFSET))
-# CRAPORT1=$((1004 + OFFSET))
-# CRAPORT2=$((1005 + OFFSET))
-
 if ! [ ${OFFSET:+defined} ]; then
     OFFSET=0
 fi
@@ -37,21 +25,10 @@ PORT4=2001
 CRAPORT1=1500
 CRAPORT2=2500
 
-ME=`whoami`
+# ME=`whoami`
+ME=rrnewton
 CLIENTNAME=${ME}client${OFFSET}
 SERVERNAME=${ME}server${OFFSET}
-
-if ! which Ambrosia 2> /dev/null; then
-    echo "'Ambrosia' not found."
-    echo "You need Ambrosia on your PATH.  Please download an AMBROSIA binary distribution."
-    exit 1
-fi
-
-if ! [ -e Client1/publish/Client1.dll ]; then
-    echo "Build products don't exist in ./Client1/publish."
-    echo "Did you run ./build_dotnetcore.sh yet?"
-    exit 1
-fi
 
 echo
 echo "--------------------------------------------------------------------------------"
@@ -60,24 +37,12 @@ echo "  Instance: names $CLIENTNAME, $SERVERNAME"
 echo "--------------------------------------------------------------------------------"
 echo
 
-if ! [ ${SKIP_REGISTER:+defined} ]; then
-    set -x
-    time Ambrosia RegisterInstance -i $CLIENTNAME --rp $PORT1 --sp $PORT2 -l "./ambrosia_logs/" 
-    time Ambrosia RegisterInstance -i $SERVERNAME --rp $PORT3 --sp $PORT4 -l "./ambrosia_logs/"
-    set +x
-fi
+# Clear logs for this demonstration.
+rm -rf ./ambrosia_logs/
 
 echo "Running with AMBROSIA binaries from: "$(dirname `which runAmbrosiaService.sh`)
-slog=`mktemp server.XXXX.log`
-jlog=`mktemp client.XXXX.log`
 
-echo
-echo "PTI: Launching Server:"
-set -x
-AMBROSIA_INSTANCE_NAME=$SERVERNAME AMBROSIA_IMMORTALCOORDINATOR_PORT=$CRAPORT1 \
-COORDTAG=CoordServ AMBROSIA_IMMORTALCOORDINATOR_LOG=$slog \
-  runAmbrosiaService.sh dotnet ./Server/publish/Server.dll $SERVERNAME
-
+./run_helloworld_server.sh &
 set +x
 pid_server=$!
 echo "Server launched as PID ${pid_server}.  Waiting a bit."
@@ -89,26 +54,22 @@ if ! kill -0 $pid_server 2>/dev/null ; then
     echo
     exit 1
 fi
-echo
-echo "PTI: Launching Client1 now:"
-set -x
-AMBROSIA_INSTANCE_NAME=$CLIENTNAME AMBROSIA_IMMORTALCOORDINATOR_PORT=$CRAPORT2 \
-COORDTAG=CoordCli AMBROSIA_IMMORTALCOORDINATOR_LOG=$jlog \
-  runAmbrosiaService.sh dotnet ./Client1/Publish/Client1.dll $CLIENTNAME $SERVERNAME
-set +x
 
 echo
-echo "PTI Client finished, shutting down server."
+echo "Launching Client1 now:"
+./run_helloworld_client.sh
+
+echo
+echo "Client finished, shutting down server."
 kill $pid_server
 wait
 echo "Everything shut down.  All background processes done."
 
-echo "Attempt a cleanup of our table metadata:"
+echo "Finally, attempt a cleanup of our table metadata:"
 set -x
 UnsafeDeregisterInstance $CLIENTNAME || true
 UnsafeDeregisterInstance $SERVERNAME || true
-rm $slog $jlog
+rm *-coord.*.log
 set +x
-
 
 echo "HelloWorld all done."
