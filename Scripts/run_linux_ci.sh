fa3f50e733e833423caad15488d3fb1a104052fc
Tal Zaccai <talzacc@microsoft.com>
2019-01-16 14:21:14 -0800

    Updating HelloWorld Dockerfile

--- Scripts/run_linux_ci.sh
+++ Scripts/run_linux_ci.sh
@@ -79,7 +79,7 @@ case $mode in
       # Build Application 2: Hello World Sample
       # ----------------------------------------
       cd "$AMBROSIA_ROOT"/Samples/HelloWorld
-      echo "HelloWorld: First make sure a straight-to-the-solution build works:"
+      #echo "HelloWorld: First make sure a straight-to-the-solution build works:"
       #dotnet publish -c $CONF -f $FMWK HelloWorld.sln
       echo "HelloWorld: Then make sure it builds from scratch:"
       rm -rf GeneratedSourceFiles
