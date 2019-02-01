6c42d90b537d2b3134e66b7605e7f7fc1b86a65b
jongoldDB <42282458+jongoldDB@users.noreply.github.com>
2019-02-01 15:13:39 -0800

    Update Program.cs

--- Samples/HelloWorld/Client3/Program.cs
+++ Samples/HelloWorld/Client3/Program.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 
 namespace Client3
 {
+    // Because this Immortal makes async calls, it, and the associated generated proxies and base classes, must be
+    // compiled DEBUG. For an explanation, see HelloWorldExplained.
     [DataContract]
     class Client3 : Immortal<IClient3Proxy>, IClient3
     {
