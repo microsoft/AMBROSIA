6159ed94eba83c56a5d5f0643469ac10d12e9c94
Shannon Joyner <t-shjoyn@microsoft.com>
2019-06-10 11:52:46 -0700

    Move recovery final primary setup to a function
    For both shard and non-sharded cases, we want to correctly setup
    the primary logs. However, in the sharded case, we call
    RecoveryAsync for every shard we recover from. We don't want to
    move the upgraded directory and log file for every shard,
    so we move this final logic to a function so we can call it once
    in both sharded / non-sharded cases.

--- Ambrosia/Ambrosia/Program.cs
+++ Ambrosia/Ambrosia/Program.cs
@@ -2261,6 +2261,7 @@ namespace Ambrosia
                 Recovering = true;
                 _restartWithRecovery = true;
                 await RecoverAsync(checkpointToLoad, testUpgrade);
+                await PrepareToBecomePrimaryAsync();
                 Recovering = false;
             }
             else
@@ -2328,6 +2329,10 @@ namespace Ambrosia
                 }
                 await ReplayAsync(replayStream);
             }
+        }
+
+        private async Task PrepareToBecomePrimaryAsync()
+        {
             var readVersion = long.Parse(RetrieveServiceInfo(InfoTitle("CurrentVersion")));
             if (_currentVersion != readVersion)
             {
