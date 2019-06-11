0c5e6afc8775618e7ae57c992ebba68fca96d5ac
Shannon Joyner <t-shjoyn@microsoft.com>
2019-06-11 15:31:38 -0700

    Change more lines to use InfoTitle

--- Ambrosia/Ambrosia/Program.cs
+++ Ambrosia/Ambrosia/Program.cs
@@ -2531,7 +2531,7 @@ namespace Ambrosia
                 _checkpointWriter = new LogWriter(_logFileNameBase + "chkpt" + (_lastCommittedCheckpoint).ToString(), 1024 * 1024, 6, true);
                 _myRole = AARole.Checkpointer; // I'm a checkpointing secondary
                 var oldCheckpoint = _lastCommittedCheckpoint;
-                _lastCommittedCheckpoint = long.Parse(RetrieveServiceInfo("LastCommittedCheckpoint"));
+                _lastCommittedCheckpoint = long.Parse(RetrieveServiceInfo(InfoTitle("LastCommittedCheckpoint")));
                 if (oldCheckpoint != _lastCommittedCheckpoint)
                 {
                     _checkpointWriter.Dispose();
@@ -2556,7 +2556,7 @@ namespace Ambrosia
                     var oldLastLogFile = _lastLogFile;
                     // Compete for log write permission - non destructive open for write - open for append
                     var lastLogFileStream = new LogWriter(_logFileNameBase + "log" + (oldLastLogFile).ToString(), 1024 * 1024, 6, true);
-                    if (long.Parse(RetrieveServiceInfo("LastLogFile")) != oldLastLogFile)
+                    if (long.Parse(RetrieveServiceInfo(InfoTitle("LastLogFile"))) != oldLastLogFile)
                     {
                         // We got an old log. Try again
                         lastLogFileStream.Dispose();
@@ -2564,7 +2564,7 @@ namespace Ambrosia
                     }
                     // We got the lock! Set things up so we let go of the lock at the right moment
                     // But first check if we got the lock because the version changed, in which case, we should commit suicide
-                    var readVersion = long.Parse(RetrieveServiceInfo("CurrentVersion"));
+                    var readVersion = long.Parse(RetrieveServiceInfo(InfoTitle("CurrentVersion")));
                     if (_currentVersion != readVersion)
                     {
 
@@ -2597,7 +2597,7 @@ namespace Ambrosia
                 catch
                 {
                     // Check if the version changed, in which case, we should commit suicide
-                    var readVersion = long.Parse(RetrieveServiceInfo("CurrentVersion"));
+                    var readVersion = long.Parse(RetrieveServiceInfo(InfoTitle("CurrentVersion")));
                     if (_currentVersion != readVersion)
                     {
 
@@ -3723,11 +3723,11 @@ namespace Ambrosia
                 long readVersion = -1;
                 try
                 {
-                    readVersion = long.Parse(RetrieveServiceInfo("CurrentVersion"));
+                    readVersion = long.Parse(RetrieveServiceInfo(InfoTitle("CurrentVersion")));
                 }
                 catch
                 {
-                    OnError(VersionMismatch, "Version mismatch on process start: Expected " + _currentVersion + " was: " + RetrieveServiceInfo("CurrentVersion"));
+                    OnError(VersionMismatch, "Version mismatch on process start: Expected " + _currentVersion + " was: " + RetrieveServiceInfo(InfoTitle("CurrentVersion")));
                 }
                 if (_currentVersion != readVersion)
                 {
@@ -3735,7 +3735,7 @@ namespace Ambrosia
                 }
                 if (!_runningRepro)
                 {
-                    if (long.Parse(RetrieveServiceInfo("LastCommittedCheckpoint")) < 1)
+                    if (long.Parse(RetrieveServiceInfo(InfoTitle("LastCommittedCheckpoint"))) < 1)
                     {
                         OnError(MissingCheckpoint, "No checkpoint in metadata");
 
@@ -3745,7 +3745,7 @@ namespace Ambrosia
                 {
                     OnError(MissingCheckpoint, "No checkpoint/logs directory");
                 }
-                var lastCommittedCheckpoint = long.Parse(RetrieveServiceInfo("LastCommittedCheckpoint"));
+                var lastCommittedCheckpoint = long.Parse(RetrieveServiceInfo(InfoTitle("LastCommittedCheckpoint")));
                 if (!LogWriter.FileExists(Path.Combine(_serviceLogPath + _serviceName + "_" + _currentVersion,
                                                        "server" + "chkpt" + lastCommittedCheckpoint)))
                 {
