b86e29dec94d22385317ef7c561f735d2da9b715
Shannon Joyner <t-shjoyn@microsoft.com>
2019-06-13 16:41:14 -0700

    Allow shard ID to be passed in as a parameter
    This makes it easier to switch between sharded and non-sharded
    scenario. We still need to figure out the best way to pass in
    the shards to recover from.

--- Ambrosia/Ambrosia/Program.cs
+++ Ambrosia/Ambrosia/Program.cs
@@ -1057,6 +1057,7 @@ namespace Ambrosia
         public string storageConnectionString;
         public long currentVersion;
         public long upgradeToVersion;
+        public long shardID;
     }
 
     public class AmbrosiaRuntime : VertexBase
@@ -3788,7 +3789,8 @@ namespace Ambrosia
             }
 
             bool runningRepro = false;
-            bool sharded = false;
+            bool sharded = p.shardID > 0;
+            _shardID = p.shardID;
 
             Initialize(
                 p.serviceReceiveFromPort,
@@ -3887,6 +3889,10 @@ namespace Ambrosia
             _serviceName = serviceName;
             _storageConnectionString = storageConnectionString;
             _sharded = sharded;
+            if (_sharded)
+            {
+                Console.WriteLine("Running instance with shard ID " + _shardID.ToString());
+            }
             _coral = ClientLibrary;
 
             Console.WriteLine("Logs directory: {0}", _serviceLogPath);
@@ -3950,6 +3956,7 @@ namespace Ambrosia
         private static long _logTriggerSizeMB = 1000;
         private static int _currentVersion = 0;
         private static long _upgradeVersion = -1;
+        private static long _shardID = -1;
 
         static void Main(string[] args)
         {
@@ -3988,6 +3995,7 @@ namespace Ambrosia
                     param.serviceLogPath = _serviceLogPath;
                     param.AmbrosiaBinariesLocation = _binariesLocation;
                     param.storageConnectionString = Environment.GetEnvironmentVariable("AZURE_STORAGE_CONN_STRING");
+                    param.shardID = _shardID;
 
                     try
                     {
@@ -4061,6 +4069,7 @@ namespace Ambrosia
                 {"aa|activeActive", "Is active-active enabled.", aa => _isActiveActive = true},
                 {"cv|currentVersion=", "The current version #.", cv => _currentVersion = int.Parse(cv)},
                 {"uv|upgradeVersion=", "The upgrade version #.", uv => _upgradeVersion = int.Parse(uv)},
+                {"si|shardID=", "The shard ID of the instance", si => _shardID = long.Parse(si) }
             });
 
             var addReplicaOptionSet = new OptionSet {
