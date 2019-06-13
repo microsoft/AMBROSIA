467bbc2fd78981976bf82f6387b6db52eed10cd3
Shannon Joyner <t-shjoyn@microsoft.com>
2019-06-13 16:41:14 -0700

    Allow shard ID to be passed in as a parameter
    This makes it easier to switch between sharded and non-sharded
    scenario. We still need to figure out the best way to pass in
    the shards to recover from.

--- Ambrosia/Ambrosia/Program.cs
+++ Ambrosia/Ambrosia/Program.cs
@@ -1057,6 +1057,7 @@ namespace Ambrosia
         public string storageConnectionString;
         public long currentVersion;
         public long upgradeToVersion;
+        public long shardID;
     }
 
     public static class AmbrosiaRuntimeParms
@@ -3854,7 +3855,9 @@ namespace Ambrosia
                 p = (AmbrosiaRuntimeParams)xmlSerializer.Deserialize(textReader);
             }
 
-            bool sharded = false;
+            bool runningRepro = false;
+            bool sharded = p.shardID > 0;
+            _shardID = p.shardID;
 
             Initialize(
                 p.serviceReceiveFromPort,
@@ -3951,6 +3954,10 @@ namespace Ambrosia
             _serviceName = serviceName;
             _storageConnectionString = storageConnectionString;
             _sharded = sharded;
+            if (_sharded)
+            {
+                Console.WriteLine("Running instance with shard ID " + _shardID.ToString());
+            }
             _coral = ClientLibrary;
 
             Console.WriteLine("Logs directory: {0}", _serviceLogPath);
@@ -4014,6 +4021,7 @@ namespace Ambrosia
         private static long _logTriggerSizeMB = 1000;
         private static int _currentVersion = 0;
         private static long _upgradeVersion = -1;
+        private static long _shardID = -1;
 
         static void Main(string[] args)
         {
@@ -4052,6 +4060,7 @@ namespace Ambrosia
                     param.serviceLogPath = _serviceLogPath;
                     param.AmbrosiaBinariesLocation = _binariesLocation;
                     param.storageConnectionString = Environment.GetEnvironmentVariable("AZURE_STORAGE_CONN_STRING");
+                    param.shardID = _shardID;
 
                     try
                     {
@@ -4105,6 +4114,7 @@ namespace Ambrosia
                 { "rp|receivePort=", "The service receive from port [REQUIRED].", rp => _serviceReceiveFromPort = int.Parse(rp) },
                 { "sp|sendPort=", "The service send to port. [REQUIRED]", sp => _serviceSendToPort = int.Parse(sp) },
                 { "l|log=", "The service log path.", l => _serviceLogPath = l },
+                {"si|shardID=", "The shard ID of the instance", si => _shardID = long.Parse(si) },
             };
 
             var helpOption = new OptionSet
