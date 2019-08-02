abbdd73684d4700796d8562b4ec6dbe0061fa06f
Chris Lovett <clovett@microsoft.com>
2019-08-01 19:02:31 -0700

    add net46 build to buildcore.

--- BuildCore.cmd
+++ BuildCore.cmd
@@ -3,3 +3,9 @@ dotnet publish -o %AMBROSIATOOLS%\x64\Release\netcoreapp2.0 -c Debug -f netcorea
 dotnet publish -o %AMBROSIATOOLS%\x64\Release\netcoreapp2.0 -c Debug -f netcoreapp2.0 -r win10-x64 Clients/CSharp/AmbrosiaLibCS/AmbrosiaLibCS.csproj
 dotnet publish -o %AMBROSIATOOLS%\x64\Release\netcoreapp2.0 -c Debug -f netcoreapp2.0 -r win10-x64 Clients/CSharp/AmbrosiaCS/AmbrosiaCS.csproj
 dotnet publish -o %AMBROSIATOOLS%\x64\Release\netcoreapp2.0 -c Debug -f netcoreapp2.0 -r win10-x64 DevTools/UnsafeDeregisterInstance/UnsafeDeregisterInstance.csproj
+
+dotnet publish -o %AMBROSIATOOLS%\x64\Release\net46 -c Debug -f net46 -r win10-x64 Ambrosia/Ambrosia/Ambrosia.csproj
+dotnet publish -o %AMBROSIATOOLS%\x64\Release\net46 -c Debug -f net46 -r win10-x64 ImmortalCoordinator/ImmortalCoordinator.csproj
+dotnet publish -o %AMBROSIATOOLS%\x64\Release\net46 -c Debug -f net46 -r win10-x64 Clients/CSharp/AmbrosiaLibCS/AmbrosiaLibCS.csproj
+dotnet publish -o %AMBROSIATOOLS%\x64\Release\net46 -c Debug -f net46 -r win10-x64 Clients/CSharp/AmbrosiaCS/AmbrosiaCS.csproj
+dotnet publish -o %AMBROSIATOOLS%\x64\Release\net46 -c Debug -f net46 -r win10-x64 DevTools/UnsafeDeregisterInstance/UnsafeDeregisterInstance.csproj
