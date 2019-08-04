e890d5197334d30355c88703c3dc6ca05b84c68e
Chris Lovett <clovett@Microsoft.com>
2019-08-04 13:30:27 -0700

    add buildnet46 and add building of AmbrosiaLibCS.dll

---
+++ Buildnet46.cmd
@@ -0,0 +1,5 @@
+dotnet publish -o /ambrosia/x64/Release/net46 -c Release -f net46 -r win10-x64 Ambrosia/Ambrosia/Ambrosia.csproj
+dotnet publish -o /ambrosia/x64/Release/net46 -c Release -f net46 -r win10-x64 ImmortalCoordinator/ImmortalCoordinator.csproj
+dotnet publish -o /ambrosia/x64/Release/net46 -c Release -f net46 -r win10-x64 Clients/CSharp/AmbrosiaCS/AmbrosiaCS.csproj
+dotnet publish -o /ambrosia/x64/Release/net46 -c Release -f net46 -r win10-x64 Clients/CSharp/AmbrosiaLibCS/AmbrosiaLibCS.csproj
+dotnet publish -o /ambrosia/x64/Release/net46 -c Release -f net46 -r win10-x64 DevTools/UnsafeDeregisterInstance/UnsafeDeregisterInstance.csproj
