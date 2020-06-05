d86161a15a27306c9d868aa104e2eabb8951a5f9
Jonathan Goldstein <jongold@microsoft.com>
2020-06-05 14:00:13 -0700

    Fully working AzureBlobs store! Even does active/active correctly through leases. Fixed latent bug in IC (ReadAllRequiredBytes)

--- InternalImmortals/PerformanceTestInterruptible/GeneratedSourceFiles/PTIAmbrosiaGeneratedAPI/latest/Dispatcher_IJob.cs
+++ InternalImmortals/PerformanceTestInterruptible/GeneratedSourceFiles/PTIAmbrosiaGeneratedAPI/latest/Dispatcher_IJob.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Ambrosia;
 using static Ambrosia.StreamCommunicator;
-using LocalAmbrosiaRuntime;
+using SharedAmbrosiaConstants;
 
 namespace JobAPI
 {
