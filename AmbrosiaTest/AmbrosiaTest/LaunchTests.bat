0d69be54a5a21fade8dda5783d99287809a9be4a
Darren Gehring <darrenge@microsoft.com>
2020-07-01 12:33:24 -0700

    Updated the comments

--- AmbrosiaTest/AmbrosiaTest/LaunchTests.bat
+++ AmbrosiaTest/AmbrosiaTest/LaunchTests.bat
@@ -1,17 +1,17 @@
-echo "****************************""
-echo "* Batch file to launch Ambrosia tests"
-echo "* This takes Visual Studio out of the equation"
-echo "* Keeps it simple. "
-echo "* To use this .bat file you need TestAgent to be installed:"
-echo "* https://www.visualstudio.com/downloads/?q=agents"
-echo "* "
-echo "* To run this .bat file, make sure to build the AmbrosiaTest or AmbrosiaTest_Local solution (in VS) which will"
-echo "* build AmbrosiaTest.dll and put it in the bin directory."
-echo "*"
-echo "****************************"
+rem ****************************
+rem * Batch file to launch Ambrosia tests
+rem * This takes Visual Studio out of the equation
+rem * Keeps it simple. 
+rem * To use this .bat file you need TestAgent to be installed:
+rem * https://www.visualstudio.com/downloads/?q=agents
+rem * 
+rem * To run this .bat file, make sure to build the AmbrosiaTest or AmbrosiaTest_Local solution (in VS) which will
+rem * build AmbrosiaTest.dll and put it in the bin directory.
+rem *
+rem ****************************
 
 set "testdir=%cd%"
 c:
 cd\"Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\CommonExtensions\Microsoft\TestWindow"
 vstest.console.exe %testdir%\bin\x64\Release\AmbrosiaTest.dll > AmbrosiaTestResults.txt
-echo vstest.console.exe %testdir%\AmbrosiaTest.dll /Tests:AMB_KillServer_Test
+rem vstest.console.exe %testdir%\AmbrosiaTest.dll /Tests:AMB_KillServer_Test
