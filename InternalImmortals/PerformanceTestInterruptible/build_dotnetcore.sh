f3599704d79dc1914b25f9a2410554d8d455e505
Darren Gehring <darrenge@microsoft.com>
2020-07-22 16:19:43 -0700

    Updated to handle no config or platform

--- InternalImmortals/PerformanceTestInterruptible/build_dotnetcore.sh
+++ InternalImmortals/PerformanceTestInterruptible/build_dotnetcore.sh
@@ -16,7 +16,9 @@ FMWK="${AMBROSIA_DOTNET_FRAMEWORK:-netcoreapp3.1}"
 CONF="${AMBROSIA_DOTNET_CONF:-Release}"
 
 # Use a non-absolute directory here to prevent collisions:
-BUILDIT="dotnet publish -o publish -c $CONF -f $FMWK -r $PLAT"
+BUILDIT_WITH_CONF_FMWK="dotnet publish -o publish -c $CONF -f $FMWK -r $PLAT"
+BUILDIT="dotnet publish -o publish -r $PLAT"
+
 
 echo
 echo "Build the projects that contain the RPC APIs"
@@ -37,22 +39,22 @@ echo "Generate the assemblies (assumes the AmbrosiaCS executable was built):"
 echo "----------------------------------------------------------------------"
 set -x
 # Alternatively: "dotnet ../../bin/AmbrosiaCS.dll"
-../../bin/AmbrosiaCS CodeGen -a "publish/ServerAPI.dll" -a "publish/IJob.dll" -p "API/ServerAPI.csproj" -p "IJob/IJob.csproj" -o $GENDEST -f "net46" -f "netcoreapp3.1" 
+../../bin/AmbrosiaCS CodeGen -a "publish/ServerAPI.dll" -a "publish/IJob.dll" -p "API/ServerAPI.csproj" -p "IJob/IJob.csproj" -o $GENDEST -f "net461" -f "netcoreapp3.1" 
 set +x
 
 echo
 echo "Build the generated code:"
 echo "-------------------------"
 set -x
-$BUILDIT GeneratedSourceFiles/${GENDEST}/latest/${GENDEST}.csproj
+$BUILDIT_WITH_CONF_FMWK GeneratedSourceFiles/${GENDEST}/latest/${GENDEST}.csproj
 set +x
 
 echo 
 echo "Finally, build the Job/Server executables:"
 echo "------------------------------------------"
 set -x
-$BUILDIT Client/Job.csproj
-$BUILDIT Server/Server.csproj
+$BUILDIT_WITH_CONF_FMWK Client/Job.csproj
+$BUILDIT_WITH_CONF_FMWK Server/Server.csproj
 set +x
 
 echo "$0: Finished building."
