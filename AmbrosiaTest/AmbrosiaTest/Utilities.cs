49155ab5a5175221a121dfdf357660893dfde592
Darren Gehring <darrenge@microsoft.com>
2020-09-08 15:58:20 -0700

    Created test AMB_InProc_KillServer_Test and corresponding cmp files

--- AmbrosiaTest/AmbrosiaTest/Utilities.cs
+++ AmbrosiaTest/AmbrosiaTest/Utilities.cs
@@ -1432,6 +1432,8 @@ namespace AmbrosiaTest
             Thread.Sleep(2000);
             MyUtils.CleanupAzureTables("inprockilljobtest");
             Thread.Sleep(2000);
+            MyUtils.CleanupAzureTables("inprockillservertest");
+            Thread.Sleep(2000);
 
             // Give it a few second to clean things up a bit more
             Thread.Sleep(5000);
