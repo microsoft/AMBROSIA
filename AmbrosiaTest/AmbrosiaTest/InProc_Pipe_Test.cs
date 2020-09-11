f2a942d73b6d4b3c2e260015e43366e4cc1cf8eb
Darren Gehring <darrenge@microsoft.com>
2020-09-11 15:03:02 -0700

    Updated blob calls to use shorter names. Also fixed bug in test code.

--- AmbrosiaTest/AmbrosiaTest/InProc_Pipe_Test.cs
+++ AmbrosiaTest/AmbrosiaTest/InProc_Pipe_Test.cs
@@ -1072,7 +1072,7 @@ namespace AmbrosiaTest
         public void AMB_InProc_SaveLogsToBlob_Test()
         {
             //NOTE - the Cleanup has this hard coded so if this changes, update Cleanup section too
-            string testName = "inprocsavelogtoblob";
+            string testName = "inprocblob";
             string clientJobName = testName + "clientjob";
             string serverName = testName + "server";
             string ambrosiaBlobLoc = "";// this is where you specify the name of the blob - blank is default
@@ -1146,7 +1146,7 @@ namespace AmbrosiaTest
         public void AMB_InProc_SaveLogsToFileAndBlob_Test()
         {
             //NOTE - the Cleanup has this hard coded so if this changes, update Cleanup section too
-            string testName = "inprocfileandblob";
+            string testName = "inprocfileblob";
             string clientJobName = testName + "clientjob";
             string serverName = testName + "server";
             string ambrosiaBlobLoc = testName + "blobstore\\";  // specify the name of the blob instead of taking default by making blank
@@ -1419,6 +1419,82 @@ namespace AmbrosiaTest
             MyUtils.VerifyTestOutputFileToCmpFile(logOutputFileName_Server_upgraded);
 
         }
+
+
+        //** Basic test that saves logs to blobs instead of to log files
+        [TestMethod]
+        public void AMB_DG_SandBox_Test()
+        {
+            //NOTE - the Cleanup has this hard coded so if this changes, update Cleanup section too
+            string testName = "dgsandbox";
+            string clientJobName = testName + "clientjob";
+            string serverName = testName + "server";
+            string ambrosiaBlobLoc = "";// this is where you specify the name of the blob - blank is default
+            string byteSize = "1073741824";
+
+            Utilities MyUtils = new Utilities();
+
+            //AMB1 - Job
+            string logOutputFileName_AMB1 = testName + "_AMB1.log";
+            AMB_Settings AMB1 = new AMB_Settings
+            {
+                AMB_ServiceName = clientJobName,
+                AMB_PortAppReceives = "1000",
+                AMB_PortAMBSends = "1001",
+                AMB_ServiceLogPath = ambrosiaBlobLoc,
+                AMB_CreateService = "A",
+                AMB_PauseAtStart = "N",
+                AMB_PersistLogs = "Y",
+                AMB_NewLogTriggerSize = "1000",
+                AMB_ActiveActive = "N",
+                AMB_Version = "0"
+            };
+            MyUtils.CallAMB(AMB1, logOutputFileName_AMB1, AMB_ModeConsts.RegisterInstance);
+
+            //AMB2
+            string logOutputFileName_AMB2 = testName + "_AMB2.log";
+            AMB_Settings AMB2 = new AMB_Settings
+            {
+                AMB_ServiceName = serverName,
+                AMB_PortAppReceives = "2000",
+                AMB_PortAMBSends = "2001",
+                AMB_ServiceLogPath = ambrosiaBlobLoc,
+                AMB_CreateService = "A",
+                AMB_PauseAtStart = "N",
+                AMB_PersistLogs = "Y",
+                AMB_NewLogTriggerSize = "1000",
+                AMB_ActiveActive = "N",
+                AMB_Version = "0"
+            };
+            MyUtils.CallAMB(AMB2, logOutputFileName_AMB2, AMB_ModeConsts.RegisterInstance);
+
+            //Client Job Call
+            string logOutputFileName_ClientJob = testName + "_ClientJob.log";
+            int clientJobProcessID = MyUtils.StartPerfClientJob("1001", "1000", clientJobName, serverName, "1024", "1", logOutputFileName_ClientJob,MyUtils.deployModeInProc,"1500");
+
+            //Server Call
+            string logOutputFileName_Server = testName + "_Server.log";
+            int serverProcessID = MyUtils.StartPerfServer("2001", "2000", clientJobName, serverName, logOutputFileName_Server, 1, false,0,MyUtils.deployModeInProc,"2500");
+
+            //Delay until client is done - also check Server just to make sure
+            bool pass = MyUtils.WaitForProcessToFinish(logOutputFileName_ClientJob, byteSize, 15, false, testName, true); // number of bytes processed
+            pass = MyUtils.WaitForProcessToFinish(logOutputFileName_Server, byteSize, 15, false, testName, true);
+
+            // Stop things so file is freed up and can be opened in verify
+            MyUtils.KillProcess(clientJobProcessID);
+            MyUtils.KillProcess(serverProcessID);
+
+            // Verify Client
+            MyUtils.VerifyTestOutputFileToCmpFile(logOutputFileName_ClientJob);
+
+            // Verify Server
+            MyUtils.VerifyTestOutputFileToCmpFile(logOutputFileName_Server);
+
+            //** Not sure how to verify if the blob exists ... probably safe assumption that if client and server get the data,
+            //** Then safe to say that blob worked. 
+        }
+
+
 
         [TestCleanup()]
         public void Cleanup()
