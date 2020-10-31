8511753d0ddcd40fae927c2fd0e1998d456a77bb
Darren Gehring <darrenge@microsoft.com>
2020-10-30 17:15:43 -0700

    Another step closer getting JS Code Gen tests in place

---
+++ AmbrosiaTest/AmbrosiaTest/JS_CodeGen_Tests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading;
+using System.Windows.Forms; // need this to handle threading issue on sleeps
+using System.Configuration;
+
+
+namespace AmbrosiaTest
+{
+    [TestClass]
+    public class JS_CodeGen_Tests
+    {
+
+        //************* Init Code *****************
+        // NOTE: Build the javascript test app once at beginning of the class.
+        [ClassInitialize()]
+        public static void Class_Initialize(TestContext tc)
+        {
+            // Build the JS app first from a JS file
+            JS_Utilities JSUtils = new JS_Utilities();
+//*#*#*#*#            JSUtils.BuildJSTestApp();
+        }
+
+        // NOTE: Make sure all names be "Azure Safe". No capital letters and no underscore.
+        [TestInitialize()]
+        public void Initialize()
+        {
+            Utilities MyUtils = new Utilities();
+            MyUtils.TestInitialize();
+        }
+        //************* Init Code *****************
+
+        [TestMethod]
+        public void JS_CodeGen_UnitTest()
+        {
+            Utilities MyUtils = new Utilities();
+            JS_Utilities JSUtils = new JS_Utilities();
+
+            string testfileName = "PI.ts";
+            JSUtils.Test_CodeGen_TSFile(testfileName);
+
+            string DG = "Done!";
+
+        }
+    }
+}
