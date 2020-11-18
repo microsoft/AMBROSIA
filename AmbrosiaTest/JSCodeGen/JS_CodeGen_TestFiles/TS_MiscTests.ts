96837897d6d1a8149802eeaf43789d08487ffd48
Darren Gehring <darrenge@microsoft.com>
2020-11-18 11:39:29 -0800

    JS Code Gen tests - Added misc valid tests and negative test for invalid type that has embedded comments

---
+++ AmbrosiaTest/JSCodeGen/JS_CodeGen_TestFiles/TS_MiscTests.ts
@@ -0,0 +1,29 @@
+/** 
+   Test File of misc tests. If find a theme or grouping then move out of this file into separate file
+*/
+
+export namespace Test {
+
+
+    /** 
+     * Correctly handle line-breaks and comments
+     * @ambrosia publish=true 
+     */
+    export function myComplexReturnFunction(): 
+    {
+        // TEST0
+        r1: string,
+        r2:  
+        // TEST1
+        /* 
+        TEST2
+        */
+        string
+    }
+    {
+        return (null);
+    }
+
+
+
+}
