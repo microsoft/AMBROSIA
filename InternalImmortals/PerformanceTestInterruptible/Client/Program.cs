3f0ea993d7cc9e4f629f9981795674fbea476b60
Sekwon Lee <t-sekwonlee@microsoft.com>
2021-08-06 15:59:28 -0500

    Disable the print statements shown when checking the orders of received messages in Release mode

--- InternalImmortals/PerformanceTestInterruptible/Client/Program.cs
+++ InternalImmortals/PerformanceTestInterruptible/Client/Program.cs
@@ -142,7 +142,9 @@ namespace Job
             var curCallNum = StreamCommunicator.ReadBufferedLong(arg, 0);
             if (_lastCallNum + 1 != curCallNum)
             {
-                //Console.WriteLine("*X* Out of order message. Expected {0}, got {1}", _lastCallNum + 1, curCallNum);
+#if DEBUG
+                Console.WriteLine("*X* Out of order message. Expected {0}, got {1}", _lastCallNum + 1, curCallNum);
+#endif
             }
             _lastCallNum = curCallNum;
         }
