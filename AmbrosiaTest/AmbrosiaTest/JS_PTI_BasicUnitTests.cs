4d8545b1d410c0c04e9cc1dff9688f2e1e4604d4
Darren Gehring <darrenge@microsoft.com>
2021-08-16 15:29:21 -0700

    Got JS_UpdateJSConfigFile working

--- AmbrosiaTest/AmbrosiaTest/JS_PTI_BasicUnitTests.cs
+++ AmbrosiaTest/AmbrosiaTest/JS_PTI_BasicUnitTests.cs
@@ -20,7 +20,7 @@ namespace AmbrosiaTest
             JS_Utilities JSUtils = new JS_Utilities();
 
             MyUtils.TestInitialize();
-            JSUtils.BuildJSTestApp();
+            //JSUtils.BuildJSTestApp(); -- maybe don't do this - not needed to build every time ... could assume it is built as well
         }
         //************* Init Code *****************
 
@@ -30,19 +30,28 @@ namespace AmbrosiaTest
         {
             // Kill all exes associated with tests
             JS_Utilities JSUtils = new JS_Utilities();
-            JSUtils.JS_PTI_TestCleanup();
+            JSUtils.JS_TestCleanup();
         }
 
         [TestMethod]
-        public void JS_PTI_BasicEndToEnd_Test()
+        public void JS_PTI_BasicBiDiEndToEnd_Test()
         {
+            // ** Probably set in Init
+            // ** Set AUtoregister = true, Log dirs, binary directory
 
             Utilities MyUtils = new Utilities();
             JS_Utilities JSUtils = new JS_Utilities();
 
-            string testName = "jsptiendtoendtest";
+            string testName = "jsptibidiendtoendtest";
             string logOutputFileName_TestApp = testName + "_TestApp.log";
+            JSUtils.JS_UpdateJSConfigFile(JSUtils.JSConfig_instanceName, testName);
 
+
+            JSUtils.StartJSTestApp(JSUtils.JSPTI_CombinedInstanceRole, logOutputFileName_TestApp);
+
+            bool pass = MyUtils.WaitForProcessToFinish(logOutputFileName_TestApp, "Bytes received: 256", 5, false, testName, true); // number of bytes processed
+            pass = MyUtils.WaitForProcessToFinish(logOutputFileName_TestApp, "SUCCESS: The expected number of bytes (256) have been received", 5, false, testName, true); // number of bytes processed
+            pass = MyUtils.WaitForProcessToFinish(logOutputFileName_TestApp, "SUCCESS: The expected number of echoed bytes (256) have been received", 5, false, testName, true); // number of bytes processed
 
         }
     }
