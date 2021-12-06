121d949aa3db445c3322a4fbb12a0e4b96004771
Jonathan Goldstein <jongold@microsoft.com>
2021-12-06 13:17:40 -0800

    trying new log record integrity check

--- AmbrosiaLib/Ambrosia/Program.cs
+++ AmbrosiaLib/Ambrosia/Program.cs
@@ -1487,17 +1487,178 @@ namespace Ambrosia
                             int offset,
                             int length)
             {
-                ulong checkBytes = FNV_offset_basis;
-                fixed (byte* p = bufToCalc)
+                /*                ulong checkBytes = FNV_offset_basis;
+                                fixed (byte* p = bufToCalc)
+                                {
+                                    for (int i = offset; i < offset + length; i++)
+                                    {
+                                        checkBytes ^= p[i];
+                                        checkBytes *= FNV_prime;
+                                    }
+                                }
+                                return checkBytes;*/
+
+                fixed (byte* pData = &bufToCalc[offset])
                 {
-                    for (int i = offset; i < offset + length; i++)
+                    return xxHash64.UnsafeComputeHash(pData, length);
+                }
+
+            }
+
+            public static partial class xxHash64
+            {
+                private const ulong p1 = 11400714785074694791UL;
+                private const ulong p2 = 14029467366897019727UL;
+                private const ulong p3 = 1609587929392839161UL;
+                private const ulong p4 = 9650029242287828579UL;
+                private const ulong p5 = 2870177450012600261UL;
+
+                internal static class BitUtils
+                {
+                    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+                    public static uint RotateLeft(uint value, int offset)
                     {
-                        checkBytes ^= p[i];
-                        checkBytes *= FNV_prime;
+#if FCL_BITOPS
+            return System.Numerics.BitOperations.RotateLeft(value, offset);
+#else
+                        return (value << offset) | (value >> (32 - offset));
+#endif
+                    }
+
+                    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+                    public static ulong RotateLeft(ulong value, int offset) // Taken help from: https://stackoverflow.com/a/48580489/5592276
+                    {
+#if FCL_BITOPS
+            return System.Numerics.BitOperations.RotateLeft(value, offset);
+#else
+                        return (value << offset) | (value >> (64 - offset));
+#endif
                     }
                 }
-                return checkBytes;
+
+                /// <summary>
+                /// Compute xxhash64 for the unsafe array of memory
+                /// </summary>
+                /// <param name="ptr"></param>
+                /// <param name="length"></param>
+                /// <param name="seed"></param>
+                /// <returns></returns>
+                [MethodImpl(MethodImplOptions.AggressiveInlining)]
+                internal static unsafe ulong UnsafeComputeHash(byte* ptr, int length, ulong seed = 0)
+                {
+                    byte* end = ptr + length;
+                    ulong h64;
+
+                    if (length >= 32)
+
+                    {
+                        byte* limit = end - 32;
+
+                        ulong v1 = seed + p1 + p2;
+                        ulong v2 = seed + p2;
+                        ulong v3 = seed + 0;
+                        ulong v4 = seed - p1;
+
+                        do
+                        {
+                            v1 += *((ulong*)ptr) * p2;
+                            v1 = BitUtils.RotateLeft(v1, 31); // rotl 31
+                            v1 *= p1;
+                            ptr += 8;
+
+                            v2 += *((ulong*)ptr) * p2;
+                            v2 = BitUtils.RotateLeft(v2, 31); // rotl 31
+                            v2 *= p1;
+                            ptr += 8;
+
+                            v3 += *((ulong*)ptr) * p2;
+                            v3 = BitUtils.RotateLeft(v3, 31); // rotl 31
+                            v3 *= p1;
+                            ptr += 8;
+
+                            v4 += *((ulong*)ptr) * p2;
+                            v4 = BitUtils.RotateLeft(v4, 31); // rotl 31
+                            v4 *= p1;
+                            ptr += 8;
+
+                        } while (ptr <= limit);
+
+                        h64 = BitUtils.RotateLeft(v1, 1) +  // rotl 1
+                              BitUtils.RotateLeft(v2, 7) +  // rotl 7
+                              BitUtils.RotateLeft(v3, 12) + // rotl 12
+                              BitUtils.RotateLeft(v4, 18);  // rotl 18
+
+                        // merge round
+                        v1 *= p2;
+                        v1 = BitUtils.RotateLeft(v1, 31); // rotl 31
+                        v1 *= p1;
+                        h64 ^= v1;
+                        h64 = h64 * p1 + p4;
+
+                        // merge round
+                        v2 *= p2;
+                        v2 = BitUtils.RotateLeft(v2, 31); // rotl 31
+                        v2 *= p1;
+                        h64 ^= v2;
+                        h64 = h64 * p1 + p4;
+
+                        // merge round
+                        v3 *= p2;
+                        v3 = BitUtils.RotateLeft(v3, 31); // rotl 31
+                        v3 *= p1;
+                        h64 ^= v3;
+                        h64 = h64 * p1 + p4;
+
+                        // merge round
+                        v4 *= p2;
+                        v4 = BitUtils.RotateLeft(v4, 31); // rotl 31
+                        v4 *= p1;
+                        h64 ^= v4;
+                        h64 = h64 * p1 + p4;
+                    }
+                    else
+                    {
+                        h64 = seed + p5;
+                    }
+
+                    h64 += (ulong)length;
+
+                    // finalize
+                    while (ptr <= end - 8)
+                    {
+                        ulong t1 = *((ulong*)ptr) * p2;
+                        t1 = BitUtils.RotateLeft(t1, 31); // rotl 31
+                        t1 *= p1;
+                        h64 ^= t1;
+                        h64 = BitUtils.RotateLeft(h64, 27) * p1 + p4; // (rotl 27) * p1 + p4
+                        ptr += 8;
+                    }
+
+                    if (ptr <= end - 4)
+                    {
+                        h64 ^= *((uint*)ptr) * p1;
+                        h64 = BitUtils.RotateLeft(h64, 23) * p2 + p3; // (rotl 23) * p2 + p3
+                        ptr += 4;
+                    }
+
+                    while (ptr < end)
+                    {
+                        h64 ^= *((byte*)ptr) * p5;
+                        h64 = BitUtils.RotateLeft(h64, 11) * p1; // (rotl 11) * p1
+                        ptr += 1;
+                    }
+
+                    // avalanche
+                    h64 ^= h64 >> 33;
+                    h64 *= p2;
+                    h64 ^= h64 >> 29;
+                    h64 *= p3;
+                    h64 ^= h64 >> 32;
+
+                    return h64;
+                }
             }
+
 
             internal unsafe ulong CheckBytes(int offset,
                                             int length)
