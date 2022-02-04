34c693eff81d95d327d9849a534e98d289a24025
Darren Gehring <darrenge@microsoft.com>
2022-02-04 11:34:19 -0800

    Cleaned up the build_windows_ci.sh

--- Scripts/build_windows_ci.sh
+++ Scripts/build_windows_ci.sh
@@ -22,28 +22,13 @@ source Scripts/ci_common_defs.sh
 echo "Executing a native Windows, non-Docker build."
 export AMBROSIA_ROOT=`pwd`
 export PATH="$PATH:$AMBROSIA_ROOT/bin"
-# Do we need this - builds core so shouldn't need it?
-#./build_dotnetcore_bindist.sh  
+./build_dotnetcore_bindist.sh  
 
 # Build PTI
 # ----------------------------------------
 cd "$AMBROSIA_ROOT"/InternalImmortals/PerformanceTestInterruptible
 ./build_dotnetcore.sh
 
-# Build Application 2: Hello World Sample
-# ----------------------------------------
-#cd "$AMBROSIA_ROOT"/Samples/HelloWorld
-#./build_dotnetcore.sh || echo "EXPECTED FAILURE - problems with Hello World net461 for now"
-
 # ----------------------------------------
 echo
 echo "All builds completed."
-#if ! [[ ${AZURE_STORAGE_CONN_STRING:+defined} ]]; then
-#    echo "AZURE_STORAGE_CONN_STRING not defined, so not attempting runnning system tests."
-#    exit 0
-#fi
-
-# Test Application: PTI
-# ----------------------------------------
-#cd "$AMBROSIA_ROOT"/InternalImmortals/PerformanceTestInterruptible
-#./run_small_PTI_and_shutdown.sh $INSTPREF
